1c60771f75f2f9406832410b5034b6cd3c63f25d
randust <hungmingkin@gmail.com>
2025-01-26 19:16:30 +0800

    Level-1

--- src/main/java/Lava.java
+++ src/main/java/Lava.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
 public class Lava {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello I'm Lava.");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        String indent = "    ";
+        System.out.println(indent+"____________________________________________________________");
+        System.out.println(indent+"Hello I'm Lava.");
+        System.out.println(indent+"What can I do for you?");
+        System.out.println(indent+"____________________________________________________________");
+        Scanner reader = new Scanner(System.in);
+        String input = reader.nextLine();
+        while (!input.equals("bye")){
+            System.out.println(indent+"____________________________________________________________");
+            System.out.println(indent+input);
+            System.out.println(indent+"____________________________________________________________");
+            input = reader.nextLine();
+        }
+        System.out.println(indent+"Bye. Hope to see you again soon!");
+        System.out.println(indent+"____________________________________________________________");
     }
 }
